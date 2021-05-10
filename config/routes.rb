03f5b6884e6136a22ddcbd6546847a991416f232
Erick R <erojasa07@gmail.com>
2021-05-09 21:24:41 -0600

    ADDED likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts do
     resources :comments
+    resources :likes
     end
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'about', to: 'pages#about'
